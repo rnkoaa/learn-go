2604434746d47bb9302206cc77b0647877dc7fd2
rnkoaa <richard.agyei@gmail.com>
2018-06-29 23:05:22 -0500

    sample json unmarshalling

---
+++ json2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Tentacle a character from Day of Tentacles
+type Tentacle struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func (t Tentacle) toString() string {
+	bytes, err := json.Marshal(t)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(bytes)
+}
+
+func getTentacles() []Tentacle {
+	// tentacles := make([]Tentacle, 3)
+	// raw, err := ioutil.ReadFile("./tentacles.json")
+	rawString := `[{"name":"Purple Tentacle","description":"A mutant monster and lab assistant created by mad scientist Dr. Fred Edison."},{"name":"Green Tentacle","description":"Harmless and friendly brother of Purple Tentacle."},{"name":"Bernard Bernoulli","description":"Green Tentacle's friend, he's a nerd with glasses."}]`
+	var tentacles []Tentacle
+	json.Unmarshal([]byte(rawString), &tentacles)
+	return tentacles
+}
+
+func main() {
+	tentacles := getTentacles()
+	fmt.Println(tentacles)
+	for _, te := range tentacles {
+		fmt.Println(te.toString())
+	}
+}
